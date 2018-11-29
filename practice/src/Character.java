e1f99683dd3434a3b654c8aabc964d95f5195ada
pyrojelly <42840441+pyrojelly@users.noreply.github.com>
2018-11-29 10:15:21 -0500

    added quality check

---
+++ practice/src/Character.java
@@ -0,0 +1,32 @@
+
+public class Character {
+  private String name;
+  private int health;
+
+  public Character() {
+    name = "bob";
+    health = 100;
+  }
+
+  public Character(String name, int health) {
+    this.name = name;
+    this.health = health;
+  }
+
+  public void setName(String nam) {
+    name = nam;
+  }
+
+  public void setHealth(int num) {
+    health = num;
+  }
+
+  public int getHealth() {
+    return health;
+  }
+
+  public String getname() {
+    return name;
+  }
+}
+
